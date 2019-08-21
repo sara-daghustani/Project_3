71a80571f155c6936cb7806766dc21b6d00b85fc
Eman-shgit config --global user.name Eman-shgit config --global user.name Eman-sh <amoooony@hotmail.com>
2019-08-21 19:11:38 +0300

    fix: style, button, post, homepage

--- src/EventName.js
+++ src/EventName.js
@@ -17,12 +17,12 @@ console.log(this.state.event);
 
             <button type='button' onClick={this.props.back} className='btn btn-warning mb-5 text-capitalize'>back to event list</button>
             <button type='button' onClick={this.props.getEvent} className='btn btn-warning mb-5 text-capitalize'>Go To Event</button>
-            {/* <img src={this.state.event.image} className='d-block w-100' alt='event'/> */}
+            <img src={this.state.event.image} className='d-block w-100' alt='event'/>
          
           </div>
           <div className='col-10 mx-auto col-md-6 my-3'>
       
-            <h6 className='text-uppercase'>{this.props.id}</h6>
+            <h6 className='text-uppercase'>{this.state.event.eventName}</h6>
             <h6 className='text-warning text-capitalize text-slanted'>Provided by</h6>
 
             <a href='' target='_blank' className='btn btn-primary mt-2 mx-3 text-capitalize'>Organization Webpage</a>
@@ -30,10 +30,17 @@ console.log(this.state.event);
 
             <ul className='list-group mt-4' >
             <h3 className='mt-3 mb-4'>Event Details </h3> 
-            <li className='list-group-item text-slanted'>Event Start Date : {this.props.sDate} </li> 
-            <li className='list-group-item text-slanted'>Event City {this.props.name}</li>
-            <li className='list-group-item text-slanted'>Event Category {this.props.city}</li>
-            <li className='list-group-item text-slanted'>Event Date</li>
+            {/* <li className='list-group-item text-slanted'>Event Start Date : {this.state.event.startDate} </li>  */}
+            {/* <li className='list-group-item text-slanted'>Event City {this.state.event.eventName}</li> */}
+            {/* <li className='list-group-item text-slanted'>Event Date</li> */}
+
+            <li className='list-group-item text-slanted'>Start From - { this.state.event.startDate} Until To -{this.state.event.endDate}</li> 
+            <li className='list-group-item text-slanted'>Event City - { this.state.event.eventLocation}</li>
+            <li className='list-group-item text-slanted'>Event Interest - { this.state.event.eventCategory}</li>
+            <li className='list-group-item text-slanted'>Volunteer Category - {this.state.event.volunteerCategory}</li>
+            <li className='list-group-item text-slanted'>Volunteer Number - {this.state.event.numberOfVolunteer}</li>
+            <li className='list-group-item text-slanted'>Volunteer Gender - {this.state.event.conditions}</li>
+            <li className='list-group-item text-slanted'>About The Event - {this.state.event.summary}</li>
 
             </ul>
           </div>
